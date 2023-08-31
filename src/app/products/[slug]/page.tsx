ae98a6f1264d080e6556df0645ffd603ebc0afd8
NiharMondal <developernihar@gmail.com>
2023-09-01 00:03:41 +0600

    image size updated

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,15 +1,13 @@
-import Image from "next/image";
-import { TProduct } from "@/types";
 import CartButton from "@/components/CartButton";
 import ProductRating from "@/components/Rating";
+import { TProduct } from "@/types";
+import Image from "next/image";
 
-const url = process.env.API_URL!;
+const url = process.env.API_URL;
 
-//fetch product by id
-const fetchProduct = async (id: string): Promise<TProduct | undefined> => {
-	const response = await fetch(`${url}/api/products/${id}`, {
-		cache: "no-store",
-	});
+const getProductById = async (id: string): Promise<TProduct | undefined> => {
+	const response = await fetch(`${url}/api/products/${id}`);
+
 	if (response.ok) return await response.json();
 };
 
@@ -18,12 +16,12 @@ export default async function SingleProduct({
 }: {
 	params: { slug: string };
 }) {
-	const product = await fetchProduct(params.slug!);
+	const product = await getProductById(params.slug);
 
 	return (
 		<section className=" pt-12 ">
 			<div className="grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-4 lg:gap-8">
-				<div className="border rounded-md">
+				<div className="border rounded-md w-full h-[440px]">
 					<Image
 						src={product?.image!}
 						width={300}
@@ -38,7 +36,7 @@ export default async function SingleProduct({
 						<h2 className="text-blue-500">${product?.price}</h2>
 						<ProductRating value={product?.rating.rate!} />
 					</div>
-					<CartButton pd={product ?? product!} />
+					<CartButton pd={product!} />
 					<div>
 						<h4>Product Description: </h4>
 						<p>{product?.description}</p>
