4636ec82ffbec4571b820a83d396df021c564976
NiharMondal <developernihar@gmail.com>
2024-09-14 14:13:37 +0600

    fixed: middleware routing issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,30 @@
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { decodeToken } from "./utils/decodeToken";
-const authRoutes = ["/sign-in", "/sign-up"];
+type Role = keyof typeof roleBasedPrivateRoutes;
+const AuthRoutes = ["/sign-in", "/sign-up"];
+
 const roleBasedPrivateRoutes = {
 	CUSTOMER: [/^\/account\/order/],
-	ADMIN: [/^\/dashboard\/:path/],
+	ADMIN: [/^\/dashboard\/admin/],
 };
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+	const { pathname } = request.nextUrl;
 
-type Role = keyof typeof roleBasedPrivateRoutes;
-
-export async function middleware(request:NextRequest){
-   
-    const {pathname} = request.nextUrl;
-    const accessToken  = cookies().get("accessToken")?.value;
-
-	if(accessToken && pathname.includes("/cart")){
-		return NextResponse.next()
+	const accessToken = cookies().get("accessToken")?.value;
+	if (accessToken && pathname === "/cart") {
+		return NextResponse.next();
 	}
-    if(!accessToken){
-        if(authRoutes.includes(pathname)){
-            return NextResponse.next()
-        }else{
-            return NextResponse.redirect(new URL("/sign-in", request.url));
-        }
-    }
-    let decodedData = null;
+	if (!accessToken) {
+		if (AuthRoutes.includes(pathname)) {
+			return NextResponse.next();
+		} else {
+			return NextResponse.redirect(new URL("/sign-in", request.url));
+		}
+	}
+
+	let decodedData = null;
 
 	if (accessToken) {
 		decodedData = decodeToken(accessToken) as any;
@@ -34,17 +34,16 @@ export async function middleware(request:NextRequest){
 
 	if (role && roleBasedPrivateRoutes[role as Role]) {
 		const routes = roleBasedPrivateRoutes[role as Role];
+		
 		if (routes.some((route) => pathname.match(route))) {
 			return NextResponse.next();
 		}
 	}
 
 	return NextResponse.redirect(new URL("/sign-in", request.url));
-
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-	matcher: ["/cart","/dashboard/:path*","/account/:path*"],
+	matcher: ["/cart","/account/order","/dashboard/admin"],
 };
-
