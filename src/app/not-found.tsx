24218164bcec19e4bbf624cf70c1fa9e15e9c9e0
NiharMondal <developernihar@gmail.com>
2024-09-26 23:50:30 +0600

    added: metadata in not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata:Metadata = {
+	title: "Not Found",
+	description:
+		"Requested page is not found!",
+};
 
 export default function NotFound() {
 	return (
